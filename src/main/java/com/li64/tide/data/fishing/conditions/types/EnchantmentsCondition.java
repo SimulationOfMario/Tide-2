a4028df96621ebefb5caea56f75c76fb73f48630
SimulationOfMario <imthedarkestspawn@gmail.com>
2026-09-18 15:09:40 +0200

    Improved enchantment condition when list is empty
    - Handle empty enchantment list param as a valid condition meaning "the rod must have at least one enchantment".
    - Improve version-specific implementation for 1.20 and 1.21

--- src/main/java/com/li64/tide/data/fishing/conditions/types/EnchantmentsCondition.java
+++ src/main/java/com/li64/tide/data/fishing/conditions/types/EnchantmentsCondition.java
@@ -5,18 +5,20 @@ import com.li64.tide.data.fishing.conditions.FishingCondition;
 import com.li64.tide.data.fishing.conditions.FishingConditionType;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
 //? if >=1.21 {
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.enchantment.ItemEnchantments;
-//?}
+//?} else {
+/*import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.core.registries.BuiltInRegistries;
+*///?}
 
 import java.util.List;
+import java.util.Map;
 
 public class EnchantmentsCondition extends FishingCondition {
     public static final MapCodec<EnchantmentsCondition> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
@@ -42,11 +44,21 @@ public class EnchantmentsCondition extends FishingCondition {
     @SuppressWarnings("deprecation")
     public boolean test(FishingContext context) {
         if (context.hook() == null || context.hook().rod() == null || context.hook().rod().isEmpty()) return false;
+
         //? if >=1.21 {
         ItemEnchantments enchantments = context.hook().rod().getOrDefault(DataComponents.ENCHANTMENTS, ItemEnchantments.EMPTY);
+        //?} else {
+        /*Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(context.hook().rod());
+        *///?}
+
+        // If the list of enchantments you provided is empty (or if you don't provide one),
+        // it means you simply want the fishing rod to have at least one enchantment (any enchantment will do).
+        if (this.enchantments.isEmpty()) return !enchantments.isEmpty();
+
+        //? if >=1.21 {
         return enchantments.keySet().stream().anyMatch(e -> this.enchantments.stream().anyMatch(e::is));
         //?} else {
-        /*return EnchantmentHelper.getEnchantments(context.hook().rod()).keySet().stream().anyMatch(e ->
+        /*return enchantments.keySet().stream().anyMatch(e ->
                 this.enchantments.stream().anyMatch(e2 -> e2.location().equals(BuiltInRegistries.ENCHANTMENT.getKey(e))));
         *///?}
     }
